0b28803cfa5aa66b6cd71281eccce25091b0572e
NouraAlthakafi <nouraal-thakafi@hotmail.com>
2022-01-04 17:26:11 +0000

    UI

--- ChatEver/Controllers/SignInVC.swift
+++ ChatEver/Controllers/SignInVC.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class SignInVC: UIViewController {
-
+    
+    // MARK: - Outlets
+    @IBOutlet weak var ivProfilePic: UIImageView!
+    @IBOutlet weak var tfEmail: UITextField!
+    @IBOutlet weak var tfPassword: UITextField!
+    @IBOutlet weak var btnLogIn: UIButton!
+    @IBOutlet weak var btnFacebook: UIButton!
+    
     // MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,10 +25,28 @@ class SignInVC: UIViewController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register", style: .done, target: self, action: #selector(didTapRegister))
     }
     
+    // MARK: - ViewDidAppear
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //tfEmailAttributes()
+    }
+    
     // MARK: - Functions
     @objc private func didTapRegister() {
         let vc = SignUpVC()
         navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    // MARK: - Attributes Functions
+    func tfEmailAttributes() {
+        guard let tfEmail = tfEmail else { return }
+        tfEmail.autocapitalizationType = .none
+        tfEmail.autocorrectionType = .no
+        tfEmail.returnKeyType = .continue
+        tfEmail.layer.cornerRadius = 15
+        tfEmail.layer.borderWidth = 1
+        tfEmail.layer.borderColor = UIColor.lightGray.cgColor
     }
     
 }
